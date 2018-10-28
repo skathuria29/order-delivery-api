dd41c308bae1707abf95572f3482ef6fc4236f94
Saloni Kathuria <kathuria.saloni29@gmail.com>
2018-10-29 02:38:44 +0530

    product ADD | GET routes and methods added

--- models/product.js
+++ models/product.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const uid = require('uniqid');
 
 const ProductSchema = mongoose.Schema({
     "pid": {
@@ -8,8 +9,12 @@ const ProductSchema = mongoose.Schema({
     "productName" : {
         type : String
     },
+    "description":{
+        type : String
+    },
     "quantity" : {
-        type : Number
+        type : Number,
+        required : true
     },
     "price" : {
         type : Number
@@ -17,3 +22,22 @@ const ProductSchema = mongoose.Schema({
 })
 
 let Product = module.exports = mongoose.model('Product', ProductSchema);
+
+module.exports.getProducts = function(filters, callback){
+    Product.find(filters, (err, products) => {  
+        if(err)
+            return console.error("Couldn't get products")
+        return callback(null, products);
+    })
+}
+
+module.exports.addProduct = function(product , callback){
+    var _product = new Product({
+        pid : 'pro_' + uid(),
+        productName : product.pname,
+        quantity : product.quantity,
+        price : product.price
+    })
+    _product.save(callback);
+}
+
--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const Product = require('../models/product');
+const { check } = require('express-validator/check')
 
+
+//get products
 router.get('/products' , (req,res) => {
-    res.send("get products");
+    Product.getProducts({}, (err, products) => {
+        if(err)
+            res.json(err);
+        res.json(products);
+        res.status(200);
+    })
+})
+
+//add product
+router.post('/products' , (req, res) => {
+    const pname = req.body.pname;
+    const quantity = req.body.quantity;
+    const price = req.body.price;
+
+    check('quantity').isNumeric()
+    check('price').isNumeric()
+
+    let new_product = {
+        pname : pname,
+        quantity : quantity,
+        price : price
+    }
+    Product.addProduct(new_product, (err, product) =>{
+        if(err)
+            res.json(err)
+        res.json(product)
+        res.status(200);
+    })
 })
 
 module.exports = router;
