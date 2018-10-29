55510727a65924391becd6accd3d16b27597236d
Saloni Kathuria <kathuria.saloni29@gmail.com>
2018-10-30 01:30:03 +0530

    order methods added

--- models/order.js
+++ models/order.js
@@ -10,8 +10,37 @@ const OrderSchema = mongoose.Schema({
     },
     "uid" : {
         type : String
+    },
+    "status":{
+        type : String,
+        enum : ["placed" , "ready to dispatch" , "agent assigned" , "dispatched" , "received"],
+        default : "placed"
     }
 
 })
 
 let Order = module.exports = mongoose.model('Order', OrderSchema);
+
+//all orders for inventory
+module.exports.getAllOrders = function(){
+
+}
+
+
+//get user specific orders
+module.exports.getOrders = function(){
+
+}
+
+//order -> add status to order
+module.exports.placeOrder = function(){
+
+}
+
+module.exports.orderReadyToDispatch = function(){
+
+    //send notofication to agents in vicinity
+}
+
+
+
