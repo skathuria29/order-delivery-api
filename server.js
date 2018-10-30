a57a87296827c8f3e78d299f45edcdd7832b4c5b
Saloni Kathuria <kathuria.saloni29@gmail.com>
2018-10-31 01:47:47 +0530

    cart router added

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const {PORT, MONGODB_URI} = require('./config/settings');
+const passport = require('passport');
+const expressValidator = require('express-validator');
 const bodyParser = require('body-parser');
 const products = require('./routes/product');
 const orders = require('./routes/order');
-// const cart = require('./routes/cart');
+ const cart = require('./routes/cart');
 // const status = require('./routes/status');
 
 const mongoose = require('mongoose');
@@ -16,8 +18,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+// app.use(expressValidator());
+
 app.use('/api', products);
 app.use('/api', orders);
+app.use('/api', cart);
 
 // app.use('/', (req,res) => {
 //     res.send("hello world!");
