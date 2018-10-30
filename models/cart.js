7656fed48dc558adb43d05bbd53ae8deb51a905c
Saloni Kathuria <kathuria.saloni29@gmail.com>
2018-10-31 01:46:13 +0530

    cart GET cart items | POST addToCart added

--- models/cart.js
+++ models/cart.js
@@ -1,31 +1,58 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const uid = require('uniqid');
 
 const OrderCartSchema = mongoose.Schema({
     "cid": {
         type :  String
     },
-    "items" : {
-        type : Array
+    "uid": {
+        type :  String
     },
-    "uid" : {
+    "product_id" : {
         type : String
+    },
+    "quantity" : {
+        type : Number
+    },
+    "createdDate" : {
+        type : Date
     }
 
 })
 
 let Cart = module.exports = mongoose.model('Cart', OrderCartSchema);
 
-module.exports.getCartItems = function(){
+module.exports.getCartItems = function(uid){
+    Cart.find({uid : uid},  (err, items) => {  
+        if(err)
+            return console.error("Couldn't get products")
+        return callback(null, items);
+    })
 
 }
 
-module.exports.addProductToCart = function(pid){
-    
+
+
+module.exports.deleteProductFromCart = function(product, callback){
+    Cart.deleteOne({ product_id : product.pid}, (err, product) => {
+        if(err)
+            return console.error("Couldn't delete the product");
+        callback(null, product);
+    })
 
 }
 
-module.exports.deleteProductFromCart = function(){
+module.exports.addProductToCart = function(cart, callback){
+    let cartItem = new Cart ({
+        cid : 'cart_' + uid(),
+        uid : cart.uid,
+        product_id : cart.pid,
+        quantity : cart.quantity,
+        createdDate : new Date() 
+    })
+
+    cartItem.save(callback)
 
 }
 
--- routes/cart.js
+++ routes/cart.js
@@ -1,8 +1,55 @@
 const express = require('express');
 const router = express.Router();
+const uuid = require('uniqid');
+const Cart = require('../models/cart');
+const { check , validationResult } = require('express-validator/check')
 
-router.get('/cart' , (req,res) => {
-    res.send("get cart items");
+router.get('/cart/:uid' , (req,res) => {
+    Cart.find({uid : req.params.uid} , (err, items) => {
+        if(err)
+            res.status(400).json(err);
+        res.json(items);
+        res.status(200);
+    })
 })
+
+router.post('/addToCart' , (req,res) => {
+    const pid = req.body.pid;
+    const uid = req.body.uid;
+    const quantity = req.body.quantity;
+
+    // req.checkBody(pid).notEmpty();
+    // req.checkBody(uid).notEmpty();
+    // req.checkBody(quantity).notEmpty().isNumeric();
+
+    check('pid').not().isEmpty();
+    check('uid').not().isEmpty()
+    check('quantity').isNumeric()
+
+    let errors = validationResult(req);
+    // let errors = req.validationErrors();
+
+	if (!errors.isEmpty()) {
+		res.status(400).json({ errors: errors.mapped() });
+    }
+    else{
+
+        let cart = {
+            cid : uuid(),
+            pid : pid,
+            uid : uid,
+            quantity : quantity,
+            createdDate : new Date()
+        }
+
+        Cart.addProductToCart(cart, (err, product) => {
+            if(err)
+                res.json(err)
+            res.json(product)
+            res.status(200);
+        })
+    }
+})
+
 
 module.exports = router;
