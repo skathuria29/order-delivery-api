0655dfffe56eae0bd29fcd47760114ac3e94ab07
Saloni Kathuria <kathuria.saloni29@gmail.com>
2018-11-01 23:58:52 +0530

    socket created to get and udate agent location

--- server.js
+++ server.js
@@ -7,13 +7,51 @@ const products = require('./routes/product');
 const orders = require('./routes/order');
 const cart = require('./routes/cart');
 const user = require('./routes/user');
+const inventory = require('./routes/inventory');
+const agent = require('./routes/agent');
+const Agent = require('./models/agent');
 // const status = require('./routes/status');
+
 
 const mongoose = require('mongoose');
 
 mongoose.connect(MONGODB_URI);
 
+
 const app = express();
+app.set('port', PORT )
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+server.listen(PORT, function(){
+    console.log(`Server running on port ${PORT}`);
+
+    io.on('connection', function(socket){
+        console.log("user connected");
+        socket.on('update location', function(agent){
+            const lat = location.lat;
+            const long = location.long;
+            const aid = agent.id
+            Agent.updateLocation(agent , (err, agent) => {
+                if(err)
+                    res.json(err)
+                res.json(agent);
+                res.status(200);
+            })
+        });
+    
+        socket.on('get location', (order) => {
+            const oid = order.id;
+            Agent.getLocation(order, (err, agent) => {
+                if(err)
+                    res.json(err);
+                res.json(agent);
+                res.status(200);
+            })
+        })
+    })
+})
 
 
 app.use(bodyParser.json());
@@ -47,15 +85,6 @@ app.use('/api', products);
 app.use('/api', orders);
 app.use('/api', cart);
 app.use('/api', user);
+app.use('/api', inventory);
+app.use('/api', agent);
 
-
-// app.use('/', (req,res) => {
-//     res.send("hello world!");
-// })
-
-app.listen(PORT, (err) =>{
-    if(err)
-        console.log(`PORT ${PORT} already in use`);
-    else 
-        console.log(`Server running on port ${PORT}`);
-})
