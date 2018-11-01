b643c1025bcde35308e624e4979dc7342a08bc00
Saloni Kathuria <kathuria.saloni29@gmail.com>
2018-11-01 16:28:39 +0530

    cart route added | express validator added

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const expressValidator = require('express-validator');
 const bodyParser = require('body-parser');
 const products = require('./routes/product');
 const orders = require('./routes/order');
- const cart = require('./routes/cart');
+const cart = require('./routes/cart');
+const user = require('./routes/user');
 // const status = require('./routes/status');
 
 const mongoose = require('mongoose');
@@ -18,14 +19,35 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Passport init
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.use(expressValidator());
+
+
+// Express Validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+  
+      while(namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+        param : formParam,
+        msg   : msg,
+        value : value
+      };
+    }
+  }));
 
 app.use('/api', products);
 app.use('/api', orders);
 app.use('/api', cart);
+app.use('/api', user);
+
 
 // app.use('/', (req,res) => {
 //     res.send("hello world!");
